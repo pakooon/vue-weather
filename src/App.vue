ca4d75865c2db293eede019d568c98b38693c9c5
pakooon <rapbby@naver.com>
2017-09-18 22:12:24 +0900

    ss

---
+++ src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="app">
+    <div id="information">
+      <div id="current-weather">
+        <current-weather/>
+      </div>
+
+      <hr>
+
+      <div id="forecast">
+        <forecast></forecast>
+      </div>
+    </div>
+
+    <div id="map"> 
+      <google-map></google-map>
+    </div>
+    
+  </div>
+</template>
+
+<script>
+import CurrentWeather from './components/CurrentWeather'
+import GoogleMap from '@/components/GoogleMap'
+import Forecast from '@/components/Forecast'
+
+export default {
+  name: 'app',
+  components: {
+    CurrentWeather,
+    GoogleMap,
+    Forecast
+  }
+}
+</script>
+
+<style>
+body {
+  background-color: #fafafa;
+}
+#app {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 1000px;
+  height: 600px;
+  margin-top: -300px;
+  margin-left: -500px;
+  border-radius: 3px;
+  background-color: #444;
+  box-shadow: 0px 2px 2px 2px rgba(0, 0, 0, 0.3);
+}
+#app:after {
+  content: '';
+  display: block;
+  clear: both;
+}
+#app > #information {
+  float: left;
+  width: 600px;
+
+}
+#app > #information > #current-weather {
+  /*margin-top: 20px;*/
+  height: 200px;
+}
+hr {
+  width: 80%;
+  margin: 50px auto;
+  border: none;
+  border-top: 1px solid white;
+}
+#app > #information > #forecast {
+  height: 400px;
+}
+#app > #map {
+  float: right;
+  width: 400px;
+  height: 600px;
+}
+</style>
---
+++ src/components/CurrentWeather.vue
@@ -0,0 +1,63 @@
+<template>
+  <div>
+    <div class="location">{{location}}</div>
+    <div class="weather">{{weather}}</div>
+    <div class="temp">{{temp}}C</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'current-weather',
+  data () {
+    return {
+      location: 'Seoul',
+      weather: 'Rain',
+      temp: '30C',
+      lat: 37.566535,
+      lon: 126.977969
+    }
+  },
+  methods: {
+    setCoordinate (coordinate) {
+      this.lat = coordinate.lat
+      this.lon = coordinate.lng
+      this.axios.get('http://api.openweathermap.org/data/2.5/weather', {
+        params: {
+          lat: this.lat,
+          lon: this.lon,
+          APPID: '4364fd05ab86fe17f573ee4bafe99777'
+        }
+      })
+      .then(response => {
+        let data = response.data
+        console.log(data)
+        this.location = data.name
+        this.weather = data.weather[0].main
+        this.temp = (data.main.temp - 273.15).toFixed(0)
+      })
+    }
+  },
+  mounted () {
+    this.$bus.$on('sendCoordinate', this.setCoordinate)
+  }
+}
+</script>
+
+<style scoped>
+.location {
+  text-align: center;
+  font-size: 40pt;
+  color: white;
+}
+.weather {
+  text-align: center;
+  font-size: 20pt;
+  color: white;
+}
+.temp {
+  text-align: center;
+  font-size: 50pt;
+  color: white;
+}
+</style>
